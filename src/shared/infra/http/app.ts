12ab74300a5e41c7e28c36344977a05b88eb053c
Khalil Stemmler <metroidman12@gmail.com>
2019-10-03 19:10:36 -0400

    Update cors.

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -7,22 +7,22 @@ import compression from 'compression';
 import { v1Router } from './api/v1';
 import { isProduction } from '../../../config';
 
-const origin = {
-  origin: isProduction ? 'https://dddforum.com' : '*',
-}
+// const origin = {
+//   origin: isProduction ? 'https://dddforum.com' : '*',
+// }
 
 const app = express();
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors(origin))
+// app.use(cors(origin))
 app.use(compression())
 app.use(helmet())
 app.use(morgan('combined'))
 
 app.use('/api/v1', v1Router)
 
-const port = process.env.PORT || 9215;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
   console.log(`[App]: Listening on port ${port}`)
