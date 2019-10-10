17f7913777d336ad588ebc1e8e336029fbf612a8
Khalil Stemmler <metroidman12@gmail.com>
2019-10-10 05:40:15 -0400

    Update front-end api.

--- public/app/src/config/api.tsx
+++ public/app/src/config/api.tsx
@@ -1,6 +1,14 @@
 
-const apiConfig = {
-  baseUrl: 'http://localhost:5000/api/v1'
+const isProduction = process.env.NODE_ENV === 'production'
+
+const devApiConfig = {
+  baseUrl: 'https://localhost:5000/api/v1'
 }
+
+const prodApiConfig = {
+  baseUrl: 'https://dddforum.herokuapp.com/api/v1'
+}
+
+const apiConfig = isProduction ? prodApiConfig : devApiConfig;
 
 export { apiConfig };
