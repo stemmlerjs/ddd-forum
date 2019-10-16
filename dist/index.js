95d071c7182472d238f5a834fad3c101aede8ad3
Anthony Denneulin <a.denneulin@gmail.com>
2019-10-16 14:49:15 +0200

    remove /dist folder

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// Infra
-require("./shared/infra/http/app");
-require("./shared/infra/database/sequelize");
-// Subscriptions
-require("./modules/forum/subscriptions");
-//# sourceMappingURL=index.js.map
--- dist/modules/forum/domain/services/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const postService_1 = require("./postService");
-const postService = new postService_1.PostService();
-exports.postService = postService;
-//# sourceMappingURL=index.js.map
