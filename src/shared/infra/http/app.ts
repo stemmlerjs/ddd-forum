e8a6f04d203e3a71c45cc3737e8367667ac51388
Trevor Blades <tdblades@gmail.com>
2019-12-04 14:10:12 -0800

    Add apollo server

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -6,6 +6,9 @@ import helmet from 'helmet';
 import compression from 'compression';
 import { v1Router } from './api/v1';
 import { isProduction } from '../../../config';
+import { ApolloServer, gql } from 'apollo-server-express';
+import { getRecentPosts } from '../../../modules/forum/useCases/post/getRecentPosts'
+import { PostDetailsMap } from '../../../modules/forum/mappers/postDetailsMap'
 
 const origin = {
   // origin: isProduction ? 'https://dddforum.com' : '*',
@@ -22,6 +25,37 @@ app.use(helmet())
 app.use(morgan('combined'))
 
 app.use('/api/v1', v1Router)
+
+const typeDefs = gql`
+  type Query {
+    posts: [Post]
+  }
+
+  type Post {
+    slug: String
+    title: String
+    text: String
+  }
+`;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers: {
+    Query: {
+      posts: async (parent, args, context) => {
+        const response = await getRecentPosts.execute({ });
+        if (response.isRight()) {
+          const postDetails = response.value.getValue();
+          return postDetails.map(PostDetailsMap.toDTO);
+        } else {
+          throw response.value;
+        }
+      }
+    }
+  }
+});
+
+server.applyMiddleware({ app });
 
 const port = process.env.PORT || 5000;
 
