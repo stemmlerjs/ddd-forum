87d810fff05f0a8428f06c6b45b35a0df17e12e0
Khalil Stemmler <metroidman12@gmail.com>
2021-04-13 18:36:01 -0400

    Cleaned up tests.

--- projects/frontend/src/pages/index/index.spec.tsx
+++ projects/frontend/src/pages/index/index.spec.tsx
@@ -10,7 +10,6 @@ describe('Home page', () => {
   describe('Given there are 4 posts', () => {
     describe('When the user lands on the index page', () => {
       test('Then they should see all four posts ranked by popularity', async () => {
-        jest.useFakeTimers();
         const usersService = new MockUserService(null);
         const postsService = new MockPostService(
           [
@@ -103,18 +102,3 @@ describe('Home page', () => {
    */
   
 })
-
-
-/**
- * @name IndexPageAcceptanceTest
- * @type Integration
- * @desc Pages contain features. Features are user stories, use cases, commands, or queries.
- * Features are known to have one or more acceptance tests. Therefore, for each page, we'll 
- * write the acceptance tests to test each feature. These will be the highest level tests.
- * They are going to be "unit" tests (though Kent C. Dodds, a popular React educator, calls them
- * integration tests - where integration tests are actually synonymous to "contract" tests, testing 
- * code you don't own). We're only interested in testing code we own. And we want these tests to
- * run fast. There will be gaps, but we will fill those with "integration" and (component-level)
- * "unit" tests later.
- * 
- */
