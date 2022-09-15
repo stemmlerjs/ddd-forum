c45b4b1baac45bd89d880480ab7e9108dc93ad09
Jagdish Parihar <jatin6972@gmail.com>
2022-09-15 23:55:23 +0530

    Update v1.ts

--- src/shared/infra/http/api/v1.ts
+++ src/shared/infra/http/api/v1.ts
@@ -1,8 +1,7 @@
 
 import express from 'express'
 import { userRouter } from '../../../../modules/users/infra/http/routes';
-import { memberRouter, commentRouter } from '../../../../modules/forum/infra/http/routes';
-import { postRouter } from '../../../../modules/forum/infra/http/routes/post';
+import { memberRouter, commentRouter, postRouter } from '../../../../modules/forum/infra/http/routes';
 
 const v1Router = express.Router();
 
